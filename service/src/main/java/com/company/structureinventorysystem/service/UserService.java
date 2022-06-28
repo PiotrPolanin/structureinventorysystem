316f58abfa732a2ba25dfd36c4026ce146915432
Piotr Polanin <polaninp@gmail.com>
2022-06-28 13:41:13 +0200

    Added getByIdWithRoles method in UserService class in service module

--- service/src/main/java/com/company/structureinventorysystem/service/UserService.java
+++ service/src/main/java/com/company/structureinventorysystem/service/UserService.java
@@ -6,7 +6,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService extends GenericService<User> {
+
+    private final UserRepository repository;
+
     public UserService(UserRepository repository) {
-        super(repository);
+        super(User.class, repository);
+        this.repository = repository;
     }
+
+    public User getByIdWithRoles(Long id) {
+        return repository.findByIdWithRoles(id);
+    }
+
 }
