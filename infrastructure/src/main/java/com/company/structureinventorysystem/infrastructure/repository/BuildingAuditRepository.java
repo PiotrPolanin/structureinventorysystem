c47a19f162631f41f346abaad513e8135a0b617f
Piotr Polanin <polaninp@gmail.com>
2022-07-15 15:43:50 +0200

    Added methods to building audit repository to find entities by dates of creation and update in infrastructure module

--- infrastructure/src/main/java/com/company/structureinventorysystem/infrastructure/repository/BuildingAuditRepository.java
+++ infrastructure/src/main/java/com/company/structureinventorysystem/infrastructure/repository/BuildingAuditRepository.java
@@ -1,8 +1,25 @@
 package com.company.structureinventorysystem.infrastructure.repository;
 
 import com.company.structureinventorysystem.domain.audit.BuildingAudit;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BuildingAuditRepository extends GenericRepository<BuildingAudit> {
+
+    @Query("SELECT ba FROM BuildingAudit ba ORDER BY ba.createdOn ASC")
+    List<BuildingAudit> findAllByCreatedOnAscOrder(Pageable pageable);
+
+    @Query("SELECT ba FROM BuildingAudit ba ORDER BY ba.createdOn DESC")
+    List<BuildingAudit> findAllByCreatedOnDescOrder(Pageable pageable);
+
+    @Query("SELECT ba FROM BuildingAudit ba ORDER BY ba.updatedOn ASC")
+    List<BuildingAudit> findAllByUpdatedOnAscOrder(Pageable pageable);
+
+    @Query("SELECT ba FROM BuildingAudit ba ORDER BY ba.updatedOn DESC")
+    List<BuildingAudit> findAllByUpdatedOnDescOrder(Pageable pageable);
+
 }
