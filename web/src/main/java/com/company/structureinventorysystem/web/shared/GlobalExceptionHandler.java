37554b7b11cbb169a1d05ef8a7ed00bde02b7eb5
Piotr Polanin <polaninp@gmail.com>
2022-07-15 16:17:50 +0200

    Added new methods in GlobalExceptionHandler class due to information about message errors for data validation in server respone in web module

--- web/src/main/java/com/company/structureinventorysystem/web/shared/GlobalExceptionHandler.java
+++ web/src/main/java/com/company/structureinventorysystem/web/shared/GlobalExceptionHandler.java
@@ -2,19 +2,36 @@ package com.company.structureinventorysystem.web.shared;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    private static final String ENTITY_NOT_FOUND_MESSAGE = "Entity was not found";
-
     @ExceptionHandler(value = EntityNotFoundException.class)
-    public ResponseEntity entityNotFoundException(EntityNotFoundException entityNotFoundException) {
-        return new ResponseEntity(ENTITY_NOT_FOUND_MESSAGE, HttpStatus.NOT_FOUND);
+    public ResponseEntity entityNotFoundException(EntityNotFoundException exception) {
+        return new ResponseEntity(exception.getLocalizedMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity httpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        return new ResponseEntity(String.format("Error to parse attribute %s", extract(exception.getLocalizedMessage())), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = IllegalStateException.class)
+    public ResponseEntity illegalStateException(IllegalStateException exception) {
+        return new ResponseEntity(exception.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    private String extract(String charSequence) {
+        Pattern attributePattern = Pattern.compile(".*\\[\"(.+)\"\\].*");
+        Matcher matcher = attributePattern.matcher(charSequence.replace("\n", ""));
+        return matcher.matches() ? matcher.group(1) : "";
     }
 
 }
