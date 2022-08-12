c7d6df3ca2b4c26140996ab1e6a0ba0d9bcf81f3
Piotr Polanin <polaninp@gmail.com>
2022-08-12 16:32:03 +0200

    Updated state of BuildingAuditControllerTestConfiguration class because of removing unnecessary @ComponentScan annotation

--- web/src/test/java/com/company/structureinventorysystem/web/configuration/BuildingAuditControllerTestConfiguration.java
+++ web/src/test/java/com/company/structureinventorysystem/web/configuration/BuildingAuditControllerTestConfiguration.java
@@ -7,11 +7,9 @@ import com.company.structureinventorysystem.service.UserService;
 import com.company.structureinventorysystem.web.audit.BuildingAuditController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@ComponentScan(basePackages = {"com.company.structureinventorysystem.service", "com.company.structureinventorysystem.infrastructure"})
 public class BuildingAuditControllerTestConfiguration {
 
     @Autowired
